da5157363ac284fd20a037b161a8d38e093232fa
luorenjie <lrj193927@gmail.com>
2020-11-19 15:50:09 -0500

    add sql data and fix UnitOfMeasure id

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -34,8 +34,11 @@ public class Recipe {
     private Difficulty difficulty;
 
     @ManyToMany
-    @JoinTable(name = "recipe_category",
-    joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    @JoinTable(
+            name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
     private Set<Category> categories;
 
     public Long getId() {
--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -1,15 +1,18 @@
 package guru.springframework.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class UnitOfMeasure {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    String name;
+    String description;
 
     public Long getId() {
         return id;
@@ -19,11 +22,11 @@ public class UnitOfMeasure {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getDescription() {
+        return description;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
